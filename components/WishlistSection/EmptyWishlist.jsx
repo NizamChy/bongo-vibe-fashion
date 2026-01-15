3ce707cb62cd6a42b3a79f6b2c0794ca04aee1ef
NizamChy <nizam.chy.cs@gmail.com>
2026-01-15 13:40:53 +0600

    updated

--- components/WishlistSection/EmptyWishlist.jsx
+++ components/WishlistSection/EmptyWishlist.jsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 const EmptyWishlist = () => {
   return (
     <>
-      <div className="min-h-content flex justify-center items-center">
+      <div className="min-h-[70vh] flex justify-center items-center">
         <div>
           <div className="max-w-md">
             <Image
               className="w-full"
-              src="/images/fashion-lifestyle/empty-wishlist.webp"
+              src="/images/empty-wishlist.webp"
               alt="empty-wishlist"
               height={500}
               width={750}
